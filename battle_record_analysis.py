82f68704214e53c365ace29aab16debff5997e99
Eric Moyer <compu.moyer@gmail.com>
2024-05-13 14:34:27 -0400

    Separate kit strengths by team
    Rather than having one set of variables,
    have a set per-team.
    
    Also, use the inverse logit function to calculate
    probability. This should be slightly faster.
    
    And explicitly model the fact that alpha does
    not win 50% of the time. Make it so that evenly
    matched teams win at the base rate for alpha.

--- battle_record_analysis.py
+++ battle_record_analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 import pymc as pm
 import pytensor.tensor as tt
 
-from battle_record import BattleRecord, BattleParticipant, KitKey
+from battle_record import BattleRecord, BattleParticipant, KitKey, TeamKey
 from itertools import chain
 from numpy.typing import NDArray
 from tqdm import tqdm
@@ -35,34 +35,29 @@ def all_kits_used(battle_records: Iterable[BattleRecord]) -> set[KitKey]:
 
 
 def single_battle_observed_kit_variables(
-    battle_record: BattleRecord, kit_index: dict[KitKey, int]
+    battle_record: BattleRecord, kit_index: dict[KitKey, int], team: TeamKey
 ) -> NDArray[np.int8]:
     """
-    Return an array of observed kit variables for a single battle record.
+    Return an array of observed kit variables for a single battle record
+    and team.
     These will be the coefficients multiplied by the kit strength factors
-    in the model.  The first half of the array will be the observed
-    kit variables for team alpha, and the second half will be the observed
-    kit variables for team bravo.
-
-    Importantly, the observed variables for team alpha will be negated.
-    Since the team strength is the sum of the kit factors and the
-    final probability is 1/(1+exp(bravo_strength-alpha_strength)), this
-    embeds the final subtraction in the observed variables.
+    in the model. Right now, they are the count of the number of occurrences
+    of the kit in the team. Later will have different factors for the second
+    and further appearances of a kit (or a main) on a team.
 
     Args:
         battle_record: The battle record to examine.
         kit_index: A mapping from kit keys to indices.
+        team: The team to whose kits will be returned.
 
     Returns:
         An array of observed kit variables for the given battle record.
     """
     num_kits = len(kit_index)
-    a = np.zeros(2 * num_kits, dtype=np.int8)
+    a = np.zeros(num_kits, dtype=np.int8)
     for participant in battle_record.participants.values():
-        if participant.team == "alpha":
-            a[kit_index[participant.normalized_kit]] -= 1
-        else:
-            a[num_kits + kit_index[participant.normalized_kit]] += 1
+        if participant.team == team:
+            a[kit_index[participant.normalized_kit]] += 1
     return a
 
 
@@ -81,10 +76,10 @@ def single_battle_observed_alpha_win(battle_record: BattleRecord) -> NDArray[np.
 
 def all_observed_kit_variables_and_battle_results(
     battle_records: Iterable[BattleRecord], kit_index: dict[KitKey, int]
-) -> tuple[NDArray[np.int8], NDArray[np.uint8]]:
+) -> tuple[NDArray[np.int8], NDArray[np.int8], NDArray[np.uint8]]:
     """
-    Return a tuple containing two arrays: the observed kit
-    variables for all the given battle records and the results of those
+    Return a tuple containing three arrays: the observed kit
+    variables for all the given battle records for each team, and the results of those
     battles. The observed kit will be the coefficients multiplied
     by the kit strength factors in the model. The results will be
     as described in ``single_battle_observed_alpha_win``.
@@ -93,7 +88,8 @@ def all_observed_kit_variables_and_battle_results(
     so there is always a winner.)
 
     If ``rv`` the return value, then ``rv[0][i]`` will be the observed kit
-    variables for a single battle record and ``rv[1][i]`` will be the result
+    variables for the alpha team for a single battle record,
+    ``rv[1][i]`` for bravo, and ``rv[2][i]`` will be the result
     of that battle.
 
     Args:
@@ -104,16 +100,24 @@ def all_observed_kit_variables_and_battle_results(
         A tuple containing two arrays: the observed kit variables for all the given battle records
         and the results of those battles.
     """
-    observed_kit_variables = []
+    observed_kit_variables_alpha = []
+    observed_kit_variables_bravo = []
     battle_results = []
 
     for battle_record in tqdm(battle_records, desc="Processing battle records"):
-        observed_kit_variables.append(
-            single_battle_observed_kit_variables(battle_record, kit_index)
+        observed_kit_variables_alpha.append(
+            single_battle_observed_kit_variables(battle_record, kit_index, "alpha")
+        )
+        observed_kit_variables_bravo.append(
+            single_battle_observed_kit_variables(battle_record, kit_index, "bravo")
         )
         battle_results.append(single_battle_observed_alpha_win(battle_record))
 
-    return np.array(observed_kit_variables), np.array(battle_results)
+    return (
+        np.array(observed_kit_variables_alpha),
+        np.array(observed_kit_variables_bravo),
+        np.array(battle_results),
+    )
 
 
 def kit_only_model(
@@ -123,13 +127,13 @@ def kit_only_model(
     Create a PyMC3 model for the given battle record creator.
 
     Team strength is determined by the kits used by the team members.
-    let A = alpha_strength and B = bravo_strength
-    prob_team_alpha_win = exp(A)/(exp(A)+exp(B))
-    This can be rewritten as prob_team_alpha_win = 1/(1+exp(B)/exp(A)) = 1/(1+exp(B-A))
+    let A = alpha_strength and B = bravo_strength.
+    prob_team_alpha_win = exp(a)/(exp(b)+exp(a))
+    This can be rewritten as prob_team_alpha_win = (exp(a)/exp(b))/(1+(exp(a)/exp(b)))=inverse_logit(a-b)
 
     Here is the formula for each battle record:
-    kit_strength = pm.Exp("kit_strength", 1, dims=("kits",))
-    both_sides_kit_strengths = concatenate(kit_strength, kit_strength)
+    kit_strengths = pm.Exp("kit_strengths", 1, dims=("kits",))
+    both_sides_kit_strengths = concatenate(kit_strengths, kit_strengths)
     The basic formula is prob_team_alpha_win for each battle = exp(
         dot(kit_strengths, observed_kits)
     )
@@ -144,7 +148,7 @@ def kit_only_model(
     """
     kits = sorted(all_kits_used(battle_record_creator()))
     kit_index = {kit: i for i, kit in enumerate(kits)}
-    observed_kit_variables, battle_results = (
+    observed_kit_variables_alpha, observed_kit_variables_bravo, battle_results = (
         all_observed_kit_variables_and_battle_results(
             battle_record_creator(), kit_index
         )
@@ -153,45 +157,66 @@ def kit_only_model(
 
     logger.info(f"Done reading.")
     logger.info(f"Number of kits: {len(kits)}")
-    logger.info(f"Shape of observed_kit_variables: {observed_kit_variables.shape}")
+    logger.info(
+        f"Shape of observed_kit_variables_alpha: {observed_kit_variables_alpha.shape}"
+    )
+    logger.info(
+        f"Shape of observed_kit_variables_bravo: {observed_kit_variables_bravo.shape}"
+    )
     logger.info(f"Shape of battle_results: {battle_results.shape}")
 
     coords = {"kits": kits}
     with pm.Model(coords=coords) as model:
-        observed_kit_data = pm.Data(
-            "observed_kit_data", observed_kit_variables[shuffled_indices]
+        observed_kit_data_alpha = pm.Data(
+            "observed_kit_data_alpha", observed_kit_variables_alpha[shuffled_indices]
+        )
+        observed_kit_data_bravo = pm.Data(
+            "observed_kit_data_bravo", observed_kit_variables_bravo[shuffled_indices]
         )
         battle_results_data = pm.Data(
             "battle_results_data", battle_results[shuffled_indices]
         )
 
-        kit_strength = tt.reshape(
-            pm.Normal("kit_strength", mu=1, sigma=0, dims="kits"), (1, -1)
-        )
-        both_sides_kit_strengths = pm.math.concatenate(
-            [kit_strength, kit_strength], axis=1
-        )
-        logger.info(
-            f"Shape of both_sides_kit_strengths: {both_sides_kit_strengths.shape.eval()}"
-        )
+        starter_kit_strengths = pm.Normal("kit_strengths", mu=0, sigma=1, dims="kits")
+        kit_strengths = tt.reshape(starter_kit_strengths, (1, -1))
+        logger.info(f"Shape of kit_strengths: {kit_strengths.shape.eval()}")
         all_matches_kit_strengths = tt.repeat(
-            both_sides_kit_strengths,
-            observed_kit_data.shape[0],
+            kit_strengths,
+            observed_kit_data_alpha.shape[0],
             axis=0,
         )
         logger.info(
             f"Shape of all_matches_kit_strengths: {all_matches_kit_strengths.shape.eval()}"
         )
 
-        kit_contributions = all_matches_kit_strengths * observed_kit_data
-        team_beta_advantage = tt.exp(
-            tt.reshape(tt.sum(kit_contributions, axis=1), (-1, 1))
+        kit_contributions_alpha = all_matches_kit_strengths * observed_kit_data_alpha
+        kit_contributions_bravo = all_matches_kit_strengths * observed_kit_data_bravo
+        log_alpha_strength = tt.reshape(
+            tt.sum(kit_contributions_alpha, axis=1), (-1, 1)
         )
-        logger.info(f"Shape of team_beta_advantage: {team_beta_advantage.shape.eval()}")
+        log_bravo_strength = tt.reshape(
+            tt.sum(kit_contributions_bravo, axis=1), (-1, 1)
+        )
+        logger.info(f"Shape of log_alpha_strength: {log_alpha_strength.shape.eval()}")
 
-        prob_team_alpha_win = 1 / (1 + team_beta_advantage)
+        # Correct for the bias that team alpha wins more than team beta by estimating
+        # the amount that alpha wins more often. Multiply this by alpha_win_prob so that
+        # in an even match (by kit) - that is 50% by kit, alpha wins at a rate of
+        # base_win_probability
+        base_win_probability = pm.Beta("base_win_probability", alpha=0.57, beta=0.5)
         pm.Bernoulli(
-            "battle_results", prob_team_alpha_win, observed=battle_results_data
+            "base_win_likelihood", base_win_probability, observed=battle_results_data
+        )
+
+        alpha_win_prob = pm.invlogit(log_alpha_strength - log_bravo_strength)
+        # noinspection PyTypeChecker
+        adjusted_win_prob = alpha_win_prob * (base_win_probability / 0.5)
+        # Clip to between 0 and 1 (and exclude the endpoints since those can have unusual results)
+        # The right way to do this is to select the priors appropriately to keep
+        # the multiplied factor in range for the Bernoulli likelihood, but this is simple enough for now.
+        clipped_adjusted_win_prob = tt.clip(adjusted_win_prob, 2**-18, 1 - (2**-18))
+        pm.Bernoulli(
+            "battle_results", p=clipped_adjusted_win_prob, observed=battle_results_data
         )
 
     return model
